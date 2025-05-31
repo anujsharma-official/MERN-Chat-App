d4a8df3da76acc8d07b490ca7f8a5ef4a86e9bdc
Anuj Sharma <anujsharma787764@gmail.com>
2025-05-31 16:03:50 +0530

    Your descriptive commit message here

--- Backend/Frontend/vite.config.js
+++ Backend/Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://mern-chat-app-66aw.onrender.com/",
         changeOrigin: true,
       },
     },
